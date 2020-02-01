eae47351420b0912d6e2f60ad5701fad0c374483
kbbushman <k.bushman@yahoo.com>
2020-01-31 20:18:52 -0800

    Adds placeholder page

---
+++ src/pages/NewSnippet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+} from 'semantic-ui-react';
+
+const NewSnippet = () => {
+  return (
+    <Container>
+      <Header as='h2' color='yellow'>New Snippet</Header>
+    </Container>
+  )
+};
+
+export default NewSnippet;
