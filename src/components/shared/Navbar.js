864b8e9d70d37cbe98d35a0fd878754ff93a105b
kbbushman <k.bushman@yahoo.com>
2020-01-31 18:46:54 -0800

    Replaces nav buttons with icons

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -6,12 +6,13 @@ import {
   Container,
   Menu,
   Divider,
+  Icon,
 } from 'semantic-ui-react';
 
 
 const Navbar = ({ currentUser, logout }) => {
   return (
-    <Segment textAlign='center' style={{ padding: '.7em 0em .4em' }} vertical>
+    <Segment textAlign='center' style={{maxWidth: 780, padding: '.7em 0em .4em', margin: '0 auto'}} vertical>
       <Menu as='nav' inverted secondary size='large'>
         <Container>
           <Menu.Item as={Link} to='/' header style={{fontSize: 20, position: 'absolute', bottom: 25}}>
@@ -19,12 +20,22 @@ const Navbar = ({ currentUser, logout }) => {
           </Menu.Item>
           {currentUser.isLoggedIn && (
             <Menu.Item position='right'>
-              <Button as={NavLink} to='/dashboard' inverted>
+              {/* <Button as={NavLink} to='/dashboard' basic compact color='teal' style={{fontSize: 15, marginRight: '.5em'}}>
                 Dashboard
               </Button>
-              <Button inverted style={{ marginLeft: '0.5em' }} onClick={logout}>
-                Logout
+              <Button as={NavLink} to='/dashboard' basic compact color='pink' style={{fontSize: 15, marginRight: '.5em'}}>
+                +
               </Button>
+              <Button onClick={logout} basic compact color='grey' style={{fontSize: 15}}>
+                Logout
+              </Button> */}
+              <Link to='/dashboard'>
+                <Icon name='home' color='blue' style={{fontSize: 25, marginRight: '.5em', cursor: 'pointer'}} />
+              </Link>
+              <Link to='/new-snippet'>
+                <Icon name='plus' color='blue' style={{fontSize: 25, marginRight: '.5em', cursor: 'pointer'}} />
+              </Link>
+              <Icon name='sign-out' color='blue' onClick={logout} style={{fontSize: 25, cursor: 'pointer'}} />
             </Menu.Item>
           )}
           {!currentUser.isLoggedIn && (
